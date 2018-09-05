aaab279a0a02f67450a2411f9499aa5c2677b9a8
TalkL3ss <30426929+TalkL3ss@users.noreply.github.com>
2018-09-05 10:42:36 +0300

    Update ssllab-script.ps1
    Add History

--- ssllab-script.ps1
+++ ssllab-script.ps1
@@ -1,56 +1,107 @@
-$siteToCheck    = "site.co.il","www.site.co.il","site.com"
-$alljobs        = $true
-$logPath        = "c:\temp\1.csv"
+<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Scan sites rating in SSL-Labs
+.PARAMETER siteADDRs
+    Enter the sites that you want to scan as a list "www1.site1.co.il","site1.co.il".
+.PARAMETER UseProxy
+    Specifies if use proxy or not
+.PARAMETER Proxy
+    Specifies Proxy Address if not default
+.EXAMPLE
+    C:\PS> "www1.site1.co.il","site1.co.il" | Get-SiteRating
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il"
+    Scan The following sites address for rating (with proxy enabled [default])
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -UseProxy
+    Scan The following sites address for rating, with default proxy
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -UseProxy $false
+    Scan The following sites address for rating, with no proxy
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -Proxy proxy.out.local:8080
+    Scan The following sites address for rating, with other proxy settings
+.NOTES
+    Author: Ohad Halali
+    Date:   Aug 12, 2019    
+#>
+$siteToCheck    = "site1.co.il","site2.co.il","site3.com"
+$lastlogPath    = "c:\temp\1.csv"
+$logPath        = "c:\temp\" + (Get-Date -Format dd-MM-yyyy) + ".csv"
 
-New-Item -Path $logPath -Force -ItemType File
-
-Function WriteLog($line)
-{
-     Add-Content -Path $logPath -Value $line 
-} 
+New-Item -Path $logPath -Force -ItemType File | Out-Null
 
  
-Function AsyncJobs([array]$siteADDRs) {
-$sb = { param($site)
+Function Get-SiteRating {
+<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Scan sites rating in SSL-Labs
+.PARAMETER siteADDRs
+    Enter the sites that you want to scan as a list "www1.site1.co.il","site1.co.il".
+.PARAMETER UseProxy
+    Specifies if use proxy or not
+.PARAMETER Proxy
+    Specifies Proxy Address if not default
+.EXAMPLE
+    C:\PS> "www1.site1.co.il","site1.co.il" | Get-SiteRating
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il"
+    Scan The following sites address for rating (with proxy enabled [default])
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -UseProxy
+    Scan The following sites address for rating, with default proxy
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -UseProxy $false
+    Scan The following sites address for rating, with no proxy
+.EXAMPLE
+    C:\PS> Get-SiteRating -siteADDRs "www1.site1.co.il","site1.co.il" -Proxy http://proxy.out.local:8080
+    Scan The following sites address for rating, with other proxy settings
+.NOTES
+    Author: Ohad Halali
+    Date:   Aug 12, 2019    
+#>
+[CMDLetBinding()]
+param (
+    [Parameter(Mandatory=$true,Position=1,ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage="Enter the sites that you want to scan as a list `"www1.site1.co.il`",`"site1.co.il`"")]
+    $siteADDRs,
+    [string]$Proxy = "http://proxy.out.local:8080",
+    [switch]$UseProxy = $true
+)
+
+ ForEach ($site in $siteADDRs) {
         $baseSite = 'https://api.ssllabs.com/api/v3/analyze?host='
         $siteParams = '&publish=off'
-        $fullSite =  [string]::Format( "{0}{1}{2}",$baseSite,$site,$siteParams) 
-        $results = Invoke-WebRequest -Uri $fullSite | ConvertFrom-Json 
-        While ($results.status -ne "READY" -and $results.status -ne "ERROR" )
+        $fullSite =  [string]::Format("{0}{1}{2}",$baseSite,$site,$siteParams) 
+        if ($UseProxy){
+                $results = Invoke-WebRequest -Uri $fullSite -Proxy $Proxy | ConvertFrom-Json 
+            } else { 
+                $results = Invoke-WebRequest -Uri $fullSite | ConvertFrom-Json 
+            }
+        while ($results.status -ne "READY" -and $results.status -ne "ERROR" )
         {
             $siteParams = '&publish=off&fromCache=on&startNew=off&all=done'
             $fullSite =  [string]::Format( "{0}{1}{2}",$baseSite,$site,$siteParams) 
-            Start-Sleep -Seconds (Get-Random -Minimum 30 -Maximum 60) 
-            $results = Invoke-WebRequest -Uri $fullSite | ConvertFrom-Json 
-        }
-        $results | select -ExpandProperty endpoints | select @{Label="Site_Address"; exp={$site}},grade,ipAddress }
-
-    ForEach ($site in $siteADDRs) {
-        Start-Job -Name $site -ScriptBlock $sb -ArgumentList $site | out-null
-        }
-} 
-
-AsyncJobs -siteADDR $siteToCheck
-
-While ($alljobs -eq $true) { 
-    $ourJobs = Get-Job
-    foreach ($job in $ourJobs) {
-        $jobResults   = $null
-        Switch ($job.State) {
-         {$_ -eq 'Completed'} {
-                $jobResults = Receive-Job -id $job.id | Export-Csv -Path $logPath -Append -NoTypeInformation
-                Remove-Job $job.Id -Force
+            Start-Sleep -Seconds (Get-Random -Minimum 30 -Maximum 50) 
+            if ($UseProxy){
+                 $results = Invoke-WebRequest -Uri $fullSite -Proxy $Proxy | ConvertFrom-Json 
+            } else { 
+                 $results = Invoke-WebRequest -Uri $fullSite | ConvertFrom-Json 
+            } 
+            if ($results -eq $null) { 
+                $site, "Unknow","Unknow" | tee $logPath -Append
             }
-          {$_ -eq 'Failed'} {
-           }
+            else {
+                $resultsToFile =  $results | select -ExpandProperty endpoints | select @{Label="Site_Address"; exp={$site}},grade,ipAddress } 
+                $resultsToFile | Export-Csv -Path $logPath -Append -NoTypeInformation
+                $resultsToFile = $null
+                }
+        }
 
-        }      
-    } 
-    $ourJobs = $null
-    $ourJobs = Get-Job 
-
-    if ($ourJobs) {$alljobs = $true} else {$alljobs = $false}
-
-    Start-Sleep -Seconds 10
-
+    Copy-Item $logPath $lastlogPath -Force
 }
+
+Get-SiteRating -siteADDRs $siteToCheck
